11b3820a354ed776c48849246a1e959105c8c4d5
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-07 19:22:27 +0600

    add: admin path

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,44 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
+import adminRoutes from "./admin.routes";
 
 const mainRoutes = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
   },
   {
     path: "/admin",
     element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
-        path: 'dashboard',
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-    ],
+    children: adminRoutes,
+  },
+  {
+    path: "/faculty",
+    element: <MainLayout />,
+    children: adminRoutes,
+  },
+  {
+    path: "/student",
+    element: <MainLayout />,
+    children: adminRoutes,
   },
   {
     path: "/login",
