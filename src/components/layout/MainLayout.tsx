07d3c69aa72e0201e8beb3aeb6f3ae90e2079523
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-07 18:34:17 +0600

    add: layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,26 +1,24 @@
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
 import { Layout, Menu } from "antd";
-import { createElement } from "react";
 
 const MainLayout = () => {
   const { Header, Content, Footer, Sider } = Layout;
   const items = [
-    UserOutlined,
-    VideoCameraOutlined,
-    UploadOutlined,
-    UserOutlined,
-  ].map((icon, index) => ({
-    key: String(index + 1),
-    icon: createElement(icon),
-    label: `nav ${index + 1}`,
-  }));
+    {
+      key: "asfasf",
+      label: "Dashboard",
+    },
+    {
+      key: "asfasfasdf",
+      label: "Profile",
+    },
+    {
+      key: "asfasfewgr",
+      label: "User Management",
+    },
+  ];
 
   return (
-    <Layout>
+    <Layout style={{height:'100vh'}}>
       <Sider
         breakpoint="lg"
         collapsedWidth="0"
