3155ecb6f42a25cb1444e9aad26bb4e2017efd29
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-11 19:08:57 +0600

    add: un used var remove

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -2,7 +2,6 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
-import { NavLink } from "react-router-dom";
 
 
 // type TRoute = {
