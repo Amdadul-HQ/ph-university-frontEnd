85f71756b811e4fb75039e412a4b509d01da84a2
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-13 18:45:49 +0600

    set refresh token in cookies

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery(
     {
@@ -12,11 +13,31 @@ const baseQuery = fetchBaseQuery(
         }
         return headers;
     }
-    })
+})
 
+const baseQueryWithRefreshToken = async (args,api, extraOption) =>{
+    const result = await baseQuery(args,api,extraOption);
+    if(result?.error?.status === 401){
+        // * send Refresh token
+        const res = await fetch(
+          "http://localhost:5000/api/v1/auth/refresh-token",
+          {
+            method: "POST",
+            credentials: "include",
+          }
+        );
+        const data = await res.json();
+
+        const user = (api.getState() as RootState).auth.user;
+
+        api.dispatch(setUser({user,token:data?.data?.accessToken}))
+
+    }
+    return result;
+}
 
 export const baseApi = createApi({
     reducerPath:'baseApi',
-    baseQuery:baseQuery,
+    baseQuery:baseQueryWithRefreshToken,
     endpoints:() =>({})
 })
