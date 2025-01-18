2c92b5f48d203696c3d1973fd8ab82dab5efdddf
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-18 19:47:02 +0600

    fix: folder stractor

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,8 +1,5 @@
-import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
 
 const AcademicSemester = () => {
-    const {data} = useGetAllSemestersQuery(undefined);
-    console.log(data);
     return (
         <div>
             
