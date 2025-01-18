760ae91db74b8ebfcbdabbda729d3148278eb13e
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-18 18:15:22 +0600

    add: 404 error message when login

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,7 @@
 import { BaseQueryApi, BaseQueryFn, createApi, DefinitionType, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logOut, setUser } from "../features/auth/authSlice";
+import { toast } from "sonner";
 
 const baseQuery = fetchBaseQuery(
     {
@@ -17,6 +18,11 @@ const baseQuery = fetchBaseQuery(
 
 const baseQueryWithRefreshToken : BaseQueryFn<FetchArgs,BaseQueryApi,DefinitionType> = async (args,api, extraOption) :Promise<any> =>{
     let result = await baseQuery(args,api,extraOption);
+
+    if(result?.error?.status === 404){
+        toast.error("User not found!!")
+    }
+
     if(result?.error?.status === 401){
         // * send Refresh token
         const res = await fetch(
