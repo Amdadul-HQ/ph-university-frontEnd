584b74c42f9ca72d4b40c5d23c9834ac6b3647bb
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-01-18 20:39:19 +0600

    add: showing all the semester

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Table } from "antd";
 import type { TableColumnsType, TableProps } from "antd";
+import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 
 const AcademicSemester = () => {
 
+    const {data:semesterData} = useGetAllSemestersQuery(undefined);
+    const tableData = semesterData?.data.map(({_id,name,startMonth,endMonth,year}) => ({
+        _id,
+        name,
+        startMonth,
+        endMonth,
+        year
+    }))
     interface DataType {
       key: React.Key;
       name: string;
@@ -16,86 +25,22 @@ const AcademicSemester = () => {
         title: "Name",
         dataIndex: "name",
         showSorterTooltip: { target: "full-header" },
-        filters: [
-          {
-            text: "Joe",
-            value: "Joe",
-          },
-          {
-            text: "Jim",
-            value: "Jim",
-          },
-          {
-            text: "Submenu",
-            value: "Submenu",
-            children: [
-              {
-                text: "Green",
-                value: "Green",
-              },
-              {
-                text: "Black",
-                value: "Black",
-              },
-            ],
-          },
-        ],
-        // specify the condition of filtering result
-        // here is that finding the name started with `value`
-        onFilter: (value, record) => record.name.indexOf(value as string) === 0,
-        sorter: (a, b) => a.name.length - b.name.length,
-        sortDirections: ["descend"],
       },
       {
-        title: "Age",
-        dataIndex: "age",
+        title: "Year",
+        dataIndex: "year",
         defaultSortOrder: "descend",
-        sorter: (a, b) => a.age - b.age,
       },
       {
-        title: "Address",
-        dataIndex: "address",
-        filters: [
-          {
-            text: "London",
-            value: "London",
-          },
-          {
-            text: "New York",
-            value: "New York",
-          },
-        ],
-        onFilter: (value, record) =>
-          record.address.indexOf(value as string) === 0,
+        title: "Start Month",
+        dataIndex: "startMonth",
+      },
+      {
+        title: "End Month",
+        dataIndex: "endMonth",
       },
     ];
 
-    const data = [
-      {
-        key: "1",
-        name: "John Brown",
-        age: 32,
-        address: "New York No. 1 Lake Park",
-      },
-      {
-        key: "2",
-        name: "Jim Green",
-        age: 42,
-        address: "London No. 1 Lake Park",
-      },
-      {
-        key: "3",
-        name: "Joe Black",
-        age: 32,
-        address: "Sydney No. 1 Lake Park",
-      },
-      {
-        key: "4",
-        name: "Jim Red",
-        age: 32,
-        address: "London No. 2 Lake Park",
-      },
-    ];
 
     const onChange: TableProps<DataType>["onChange"] = (
       pagination,
@@ -108,7 +53,7 @@ const AcademicSemester = () => {
     return (
       <Table<DataType>
         columns={columns}
-        dataSource={data}
+        dataSource={tableData}
         onChange={onChange}
         showSorterTooltip={{ target: "sorter-icon" }}
       />
